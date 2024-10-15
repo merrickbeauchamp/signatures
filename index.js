f69a5368444e4a702c686fe763e9ee33f95d4cef
merrickbeauchamp <109994551+merrickbeauchamp@users.noreply.github.com>
2024-10-14 19:54:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,122 +1,35 @@
-console.log("Hello")
-document.getElementById('save').onclick = function () {
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-    var link = document.getElementById("link");
-    link.download = "test";
-    link.href = canvas.toDataURL("image/png");
-    link.click();
-  };
-   var canvas, ctx, flag = false,
-        prevX = 0,
-        currX = 0,
-        prevY = 0,
-        currY = 0,
-        dot_flag = false;
+let position = { x: 0, y: 0 };
+const setPosition = (e) => {
+  let rect = canvas.getBoundingClientRect();
+  position.x =
+    ((e.clientX - rect.left) / (rect.right - rect.left)) * canvas.width;
+  position.y =
+    ((e.clientY - rect.top) / (rect.bottom - rect.top)) * canvas.height;
+};
+let lines = []; //{startX, startY, endX, endY}
 
-    var x = "black",
-        y = 2;
-    
-    function init() {
-        canvas = document.getElementById('can');
-        ctx = canvas.getContext("2d");
-        w = canvas.width;
-        h = canvas.height;
-    
-        canvas.addEventListener("mousemove", function (e) {
-            findxy('move', e)
-        }, false);
-        canvas.addEventListener("mousedown", function (e) {
-            findxy('down', e)
-        }, false);
-        canvas.addEventListener("mouseup", function (e) {
-            findxy('up', e)
-        }, false);
-        canvas.addEventListener("mouseout", function (e) {
-            findxy('out', e)
-        }, false);
-    }
-    
-    function color(obj) {
-        switch (obj.id) {
-            case "green":
-                x = "green";
-                break;
-            case "blue":
-                x = "blue";
-                break;
-            case "red":
-                x = "red";
-                break;
-            case "yellow":
-                x = "yellow";
-                break;
-            case "orange":
-                x = "orange";
-                break;
-            case "black":
-                x = "black";
-                break;
-            case "white":
-                x = "white";
-                break;
-        }
-        if (x == "white") y = 14;
-        else y = 2;
-    
-    }
-    
-    function draw() {
-        ctx.beginPath();
-        ctx.moveTo(prevX, prevY);
-        ctx.lineTo(currX, currY);
-        ctx.strokeStyle = x;
-        ctx.lineWidth = y;
-        ctx.stroke();
-        ctx.closePath();
-    }
-    
-    function erase() {
-        var m = confirm("Want to clear");
-        if (m) {
-            ctx.clearRect(0, 0, w, h);
-            document.getElementById("canvasimg").style.display = "none";
-        }
-    }
-    
-    function save() {
-        document.getElementById("canvasimg").style.border = "2px solid";
-        var dataURL = canvas.toDataURL();
-        document.getElementById("canvasimg").src = dataURL;
-        document.getElementById("canvasimg").style.display = "inline";
-    }
-    
-    function findxy(res, e) {
-        if (res == 'down') {
-            prevX = currX;
-            prevY = currY;
-            currX = e.clientX - canvas.offsetLeft;
-            currY = e.clientY - canvas.offsetTop;
-    
-            flag = true;
-            dot_flag = true;
-            if (dot_flag) {
-                ctx.beginPath();
-                ctx.fillStyle = x;
-                ctx.fillRect(currX, currY, 2, 2);
-                ctx.closePath();
-                dot_flag = false;
-            }
-        }
-        if (res == 'up' || res == "out") {
-            flag = false;
-        }
-        if (res == 'move') {
-            if (flag) {
-                prevX = currX;
-                prevY = currY;
-                currX = e.clientX - canvas.offsetLeft;
-                currY = e.clientY - canvas.offsetTop;
-                draw();
-            }
-        }
-    }
+canvas.addEventListener("mousedown", setPosition);
+canvas.addEventListener("mouseenter", setPosition);
+canvas.addEventListener("mousemove", (e) => {
+  if (e.buttons !== 1) return;
+  let line = {};
+  ctx.beginPath();
+  ctx.moveTo(position.x, position.y);
+  line.startX = position.x;
+  line.startY = position.y;
+  setPosition(e);
+  ctx.lineTo(position.x, position.y);
+  line.endX = position.x;
+  line.endY = position.y;
+  ctx.stroke();
+  lines.push(line);
+});
+
+document.getElementById("clear").addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  lines = [];
+});
+
